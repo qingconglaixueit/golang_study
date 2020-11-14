f4ff3d1a907bc4dc7a9c52a50d7eb076589b5de8
qingconglaixueit <502892037@qq.com>
2020-11-14 10:46:44 +0800

    add channel goroutine grpc test path

---
+++ test/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"mystudy.com/mylib"
+)
+
+func main() {
+	fmt.Println(mylib.Add(2, 3))
+	fmt.Println(mylib.Sub(3, 1))
+}
---
+++ test/mylib/haha.go
@@ -0,0 +1,5 @@
+package mylib
+
+func Sub(a, b int) int {
+	return a - b
+}
